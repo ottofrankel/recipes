099ddd3a09decd2b5d816742cb89032335723071
Otto Frankel <ottofrankel@comcast.net>
2021-10-12 13:01:53 -0700

    removed unnecessary fetchRecipes code

--- dist/frontend/src/manage_state/action_dispatch/recipe_list_actions.js
+++ dist/frontend/src/manage_state/action_dispatch/recipe_list_actions.js
@@ -9,19 +9,6 @@ const store_1 = require("../store");
 const constants_1 = require("../../constants");
 const recipeListSlice_1 = require("../recipeListSlice");
 const favsSlice_1 = require("../favsSlice");
-// export const fetchRecipes = (query: QueryInterface): void => {
-//   let queryString: string = '?'
-//   if (query.name) queryString += 'name=' + query.name;
-//   if (query.source) queryString += '&source=' + query.source;
-//   if (query.type) queryString += '&type=' + query.type;
-//   if (query.fav) queryString += '&fav=true';
-//   if (query.tags) queryString += '&tags=' + query.tags;
-//   if (query.sort) queryString += '&sort=' + query.sort;
-//   axios.get(`${BASE_API_URL}/recipes${queryString}`)
-//   .then(res => {
-//     store.dispatch(getRecipes(res.data));
-//   })
-// }
 const fetchRecipes = (queryString) => {
     axios_1.default.get(`${constants_1.BASE_API_URL}/recipes${queryString}`)
         .then(res => {
--- src/frontend/src/manage_state/action_dispatch/recipe_list_actions.ts
+++ src/frontend/src/manage_state/action_dispatch/recipe_list_actions.ts
@@ -3,23 +3,6 @@ import { store } from '../store';
 import { BASE_API_URL } from '../../constants';
 import { getRecipes } from '../recipeListSlice';
 import { getFavs } from '../favsSlice';
-import { QueryInterface } from '../../interfaces';
-
-// export const fetchRecipes = (query: QueryInterface): void => {
-//   let queryString: string = '?'
-
-//   if (query.name) queryString += 'name=' + query.name;
-//   if (query.source) queryString += '&source=' + query.source;
-//   if (query.type) queryString += '&type=' + query.type;
-//   if (query.fav) queryString += '&fav=true';
-//   if (query.tags) queryString += '&tags=' + query.tags;
-//   if (query.sort) queryString += '&sort=' + query.sort;
-
-//   axios.get(`${BASE_API_URL}/recipes${queryString}`)
-//   .then(res => {
-//     store.dispatch(getRecipes(res.data));
-//   })
-// }
 
 export const fetchRecipes = (queryString: string): void => {
   axios.get(`${BASE_API_URL}/recipes${queryString}`)

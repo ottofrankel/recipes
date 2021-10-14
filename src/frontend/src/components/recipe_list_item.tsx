28f92142608bea5059f9f5528940bdb7ca7caf59
Otto Frankel <ottofrankel@comcast.net>
2021-10-14 14:20:54 -0700

    added dates to list items

--- src/frontend/src/components/recipe_list_item.tsx
+++ src/frontend/src/components/recipe_list_item.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box } from "@chakra-ui/layout";
+import { Box, HStack } from "@chakra-ui/layout";
 import { Link } from "react-router-dom";
 import { RecipeInterface } from "../interfaces";
 import { BASE_COLOR } from "../styles/colors";
@@ -11,11 +11,23 @@ interface Props {
 
 const RecipeListItem: React.FC<Props> = ({recipe}) => {
   return (
-    <Box maxW="sm" borderWidth="1px" borderRadius="lg" borderColor={BASE_COLOR}  overflow="hidden">
+    <Box 
+      maxW="sm"
+      borderWidth="1px" 
+      borderRadius="lg" 
+      borderColor={BASE_COLOR}  
+      overflow="hidden"
+    >
       <Box ml="1">
         <Box>
           <Link className="result-header" to={`/recipes/${recipe._id}`}>{recipe.name}</Link>
         </Box>
+
+        <HStack>
+          <p className="list-item-dates">Created on: {recipe.dateAdded?.split(',')[0]}</p>
+          {recipe.dateUpdated && <p className="list-item-dates">Last updated: {recipe.dateUpdated.split(',')[0]}</p>}
+        </HStack>
+        
         <Box className="result-source">From: {recipe.source ? recipe.source : <em>unknown</em>}</Box>
         <TagGrid recipe={recipe}/>
       </Box>    
